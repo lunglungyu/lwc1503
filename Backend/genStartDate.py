5d9cb0e62d745fe98c135c955771b2fee5c0db31
lunglungyu <lunglungyu2004@gmail.com>
2016-04-01 01:17:12 +0800

    add select Start Date

--- Backend/genStartDate.py
+++ Backend/genStartDate.py
@@ -1,3 +1,4 @@
+# run with a non leap year is *** importan *** to get standardize start date,  leap year will be fixed in other processing
 import os
 import datetime as dt
 import numpy as np
@@ -7,7 +8,7 @@ import calendar
 TSLength = 30		# Time series length
 interval = 5		# interval to take next Time Series
 yeardayMax = 365
-startYear = 2004
+startYear = 2003
 numOfYear = 10
 MAXLAG = 5
 Threshold = 0.05
@@ -17,6 +18,10 @@ oneDay = dt.timedelta(days = 1)
 keyMMDD = [] 
 firstDay = dt.date(startYear,1,1)
 periods = (yeardayMax - TSLength) / interval + 1	# no. of TS in the year
+out = open(r"startDate.csv", 'wb')
+writer = csv.writer(out, delimiter=',', quoting=csv.QUOTE_ALL)
+header = ['startDate']
+writer.writerow(header)
 for pp in range(periods):
     ppStartDay = firstDay + pp*interval*oneDay
     ppLastDay = ppStartDay + (TSLength-1)*oneDay
@@ -29,7 +34,10 @@ for pp in range(periods):
     #    y1 = ppStartDay.year
     #    m1 = ppStartDay.month
     #    d1 = ppStartDay.day
-    keyMMDD.append("%02d"%m1 + "-" + "%02d"%d1)
+    mmddStr = "%02d"%m1 + "-" + "%02d"%d1
+    row = [mmddStr]
+    writer.writerow(row)
+    keyMMDD.append(mmddStr)
     y2 = ppLastDay.year
     m2 = ppLastDay.month
     d2 = ppLastDay.day
