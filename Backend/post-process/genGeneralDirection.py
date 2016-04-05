a737999dbdb0a3e5f004df56f92d574ca7d52898
chauchakching <chauchakching@yahoo.com.hk>
2016-04-05 16:26:26 +0800

    generate general directions file

--- Backend/post-process/genGeneralDirection.py
+++ Backend/post-process/genGeneralDirection.py
@@ -14,6 +14,8 @@ import math
 # 					- 
 #
 
+
+
 yearStart = 2003
 n_year = 1
 yearEnd = yearStart+n_year-1
@@ -23,6 +25,23 @@ maxAngle = 30
 
 #execfile( 'readGCs.py' )
 #execfile( 'read76position.py' )
+
+
+# calculate correct direct distance 
+def haversine( lat1, lng1, lat2, lng2 ):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lng1, lat1, lng2, lat2 = map(radians, [lng1, lat1, lng2, lat2])
+    # haversine formula 
+    dlon = lng2 - lng1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    km = 6367 * c
+    return km
 
 def bearing ( lat1, lng1, lat2, lng2 ):
 	# in radian
@@ -47,125 +66,176 @@ def isSmallAngle( a1, a2, threshold ):
 
 # 1. find 
 # 
-all_gcData = {}
+#all_gcData = {}
 
-kk = 0
-for yy in range(yearStart, yearEnd+1):
-	for s1 in gcFromTo[yy]:
-		for mmdd in gcFromTo[yy][s1]:
-			# for a given s1, mmdd,
-			# store all GC records first
-			s1 = '548080-99999'
-			yy = 2003
-			mmdd = '7-10'
+print 'Generate general directions !'
+print 'year start:', yearStart
+print 'year end:', yearEnd
+print '---- criteria ----'
+print 'min no. of consecutive GC:', minArrowGC
+print 'max consecutive GCs angle:', maxAngle
+print '------------------'
 
-			tmp_gcData = []		# tmp_gcData[i] = [s1,s2,p1_pos, p2_pos, fromto]
-			# store all out GCs
-			for s2 in gcFromTo[yy][s1][mmdd]:
-				single_gc = {}
-				single_gc['s1'] = s1
-				single_gc['s2'] = s2
-				single_gc['p1_pos'] = pos76[s1]
-				single_gc['p2_pos'] = pos76[s2]
-				single_gc['fromto'] = True
-				tmp_gcData.append( single_gc )
-			# store all in GCs
-			for s2 in gcToFrom[yy][s1][mmdd]:
-				single_gc = {}
-				single_gc['s1'] = s1
-				single_gc['s2'] = s2
-				single_gc['p1_pos'] = pos76[s1]
-				single_gc['p2_pos'] = pos76[s2]
-				# check if there is also a out GC to s2
-				if s2 not in gcFromTo[yy][s1][mmdd]:
-					single_gc['fromto'] = False
-					tmp_gcData.append( single_gc )
+n_arrows = 0
+with open('generalDirections.txt','w') as f:
+	f.write('station,angle,angle_1,angle_2,str,fromto,dist')
+	for yy in range(yearStart, yearEnd+1):
+		#print yy
+		n_year_arrows = 0
+		for s1 in gcFromTo[yy]:
+			for mmdd in gcFromTo[yy][s1]:
+				# for a given s1, mmdd,
+				# store all GC records first
+				#s1 = '548080-99999'
+				#yy = 2003
+				#mmdd = '7-10'
 
-			# remove bidirectional from tmp_gcData first
-			i = 0
-			while i < len(tmp_gcData):
-				if tmp_gcData[i]['fromto'] == None:
-					del tmp_gcData[i]
-				else:
-					i += 1
+				tmp_gcData = []		# tmp_gcData[i] = [s1,s2,p1_pos, p2_pos, fromto]
+				# store all out GCs
+				if mmdd in gcFromTo[yy][s1]:
+					for s2 in gcFromTo[yy][s1][mmdd]:
+						single_gc = {}
+						single_gc['s1'] = s1
+						single_gc['s2'] = s2
+						single_gc['fromto'] = True
+						tmp_gcData.append( single_gc )
+				# store all in GCs
+				'''try:
+					if len(gcToFrom[yy][s1][mmdd].keys()) == 0:
+						print yy,s1,mmdd
+				except:
+					print yy,s1,mmdd'''
+				if mmdd in gcToFrom[yy][s1]:
+					for s2 in gcToFrom[yy][s1][mmdd]:
+						single_gc = {}
+						single_gc['s1'] = s1
+						single_gc['s2'] = s2
+						# check if there is also a out GC to s2
+						if s2 not in gcFromTo[yy][s1][mmdd]:
+							single_gc['fromto'] = False
+							tmp_gcData.append( single_gc )
 
-			# calculate angle
-			for single_gc in tmp_gcData:
-				lng1 = toRadian( single_gc['p1_pos']['lng'] )
-				lat1 = toRadian( single_gc['p1_pos']['lat'] )
-				lng2 = toRadian( single_gc['p2_pos']['lng'] )
-				lat2 = toRadian( single_gc['p2_pos']['lat'] )
-				single_gc['angle'] = bearing( lat1, lng1, lat2, lng2 )
-
-			tmp_gcData.sort(key=lambda rec: rec['angle'])
-			n_gcData = len(tmp_gcData)
-			tmp_gcData.extend(tmp_gcData)	# repeat the data
-
-			outGCseq = [0 for i in range(n_gcData)]
-			inGCseq = [0 for i in range(n_gcData)]
-
-			outDirs = []
-			inDirs = []
-			print n_gcData
-			# 1. mark sequence
-			for i in range(len(tmp_gcData)):
-				if tmp_gcData[i]['fromto'] == True:
-					if i == 0:
-						outGCseq[ i%n_gcData ] = 1
-						inGCseq[ i%n_gcData ] = 0
+				# remove bidirectional from tmp_gcData first
+				i = 0
+				while i < len(tmp_gcData):
+					if tmp_gcData[i]['fromto'] == None:
+						del tmp_gcData[i]
 					else:
-						previous_same_gc = (outGCseq[ (i-1)%n_gcData ] > 0)
-						a1 = tmp_gcData[i-1]['angle']
-						a2 = tmp_gcData[i]['angle']
-						if (previous_same_gc) and (not isSmallAngle(a1, a2, maxAngle)):
-							outGCseq[ i%n_gcData ] = 0
-						else:
-							outGCseq[ i%n_gcData ] = outGCseq[ (i-1)%n_gcData ] + 1
-						inGCseq[ i%n_gcData ] = 0
-				elif tmp_gcData[i]['fromto'] == False:
-					if i == 0:
-						outGCseq[ i%n_gcData ] = 0
-						inGCseq[ i%n_gcData ] = 1
-					else:
-						previous_same_gc = (inGCseq[ (i-1)%n_gcData ] > 0)
-						a1 = tmp_gcData[i-1]['angle']
-						a2 = tmp_gcData[i]['angle']
-						if (previous_same_gc) and (not isSmallAngle(a1, a2, maxAngle)):
+						i += 1
+
+				# calculate angle, direct distance
+				for single_gc in tmp_gcData:
+					single_gc['p1_pos'] = pos76[single_gc['s1']]
+					single_gc['p2_pos'] = pos76[single_gc['s2']]
+					lng1 = toRadian( single_gc['p1_pos']['lng'] )
+					lat1 = toRadian( single_gc['p1_pos']['lat'] )
+					lng2 = toRadian( single_gc['p2_pos']['lng'] )
+					lat2 = toRadian( single_gc['p2_pos']['lat'] )
+					single_gc['angle'] = bearing( lat1, lng1, lat2, lng2 )
+	  				single_gc['dist'] = haversine( lat1, lng1, lat2, lng2 )
+
+				tmp_gcData.sort(key=lambda rec: rec['angle'])
+				n_gcData = len(tmp_gcData)
+				tmp_gcData.extend(tmp_gcData)	# repeat the data
+
+				outGCseq = [0 for i in range(n_gcData)]
+				inGCseq = [0 for i in range(n_gcData)]
+
+				outDirs = []
+				inDirs = []
+				#print n_gcData
+				# 1. mark sequence
+				for i in range(len(tmp_gcData)):
+					if tmp_gcData[i]['fromto'] == True:
+						if i == 0:
+							outGCseq[ i%n_gcData ] = 1
 							inGCseq[ i%n_gcData ] = 0
 						else:
-							inGCseq[ i%n_gcData ] = inGCseq[ (i-1)%n_gcData ] + 1
-						outGCseq[ i%n_gcData ] = 0
-			print outGCseq
-			print inGCseq
-			# find valid subsequence (1,2,3,...)
-			def findGeneralDirections( gcData, seq, minCount, fromto ):
-				result = []
-				n = len(seq)
-				for i in range(len(seq)):
-					if seq[i] == 0:
-						count = seq[ (i-1+n)%n ]
-						first_angle = gcData[ (i-count+n)%n ]['angle']
-						if count >= minCount:
-							angleSum = 0
-							tmp_angle_list = []
-							for j in range(count):
-								tmp_angle = gcData[ (i-count+j+n)%n ]['angle']
-								tmp_angle_list.append( tmp_angle )
-								relative_angle = (tmp_angle + 360 - first_angle)%360
-								angleSum += relative_angle
-							angleAvg = (angleSum/count + first_angle)%360
-							datum = {}
-							datum['dir'] = angleAvg
-							datum['str'] = count
-							datum['fromto'] = fromto
-							result.append( datum )
-				return result
+							previous_same_gc = (outGCseq[ (i-1)%n_gcData ] > 0)
+							a1 = tmp_gcData[i-1]['angle']
+							a2 = tmp_gcData[i]['angle']
+							if (previous_same_gc) and (not isSmallAngle(a1, a2, maxAngle)):
+								outGCseq[ i%n_gcData ] = 0
+							else:
+								outGCseq[ i%n_gcData ] = outGCseq[ (i-1)%n_gcData ] + 1
+							inGCseq[ i%n_gcData ] = 0
+					elif tmp_gcData[i]['fromto'] == False:
+						if i == 0:
+							outGCseq[ i%n_gcData ] = 0
+							inGCseq[ i%n_gcData ] = 1
+						else:
+							previous_same_gc = (inGCseq[ (i-1)%n_gcData ] > 0)
+							a1 = tmp_gcData[i-1]['angle']
+							a2 = tmp_gcData[i]['angle']
+							if (previous_same_gc) and (not isSmallAngle(a1, a2, maxAngle)):
+								inGCseq[ i%n_gcData ] = 0
+							else:
+								inGCseq[ i%n_gcData ] = inGCseq[ (i-1)%n_gcData ] + 1
+							outGCseq[ i%n_gcData ] = 0
+				#print outGCseq
+				#print inGCseq
+				# find valid subsequence (1,2,3,...)
+				def findGeneralDirections( gcData, seq, minCount, fromto ):
+					result = []
+					n = len(seq)
+					for i in range(len(seq)):
+						if seq[i] == 0:
+							count = seq[ (i-1+n)%n ]
+							first_angle = gcData[ (i-count+n)%n ]['angle']
+							if count >= minCount:
+								angleSum = 0
+								distSum = 0
+								tmp_angle_list = []
+								for j in range(count):
+									k = (i-count+j+n)%n
+									# angle
+									tmp_angle = gcData[ k ]['angle']
+									tmp_angle_list.append( tmp_angle )
+									relative_angle = (tmp_angle + 360 - first_angle)%360
+									angleSum += relative_angle
+									# distance
+									distSum += gcData[ k ]['dist']
+								angleAvg = (angleSum/count + first_angle)%360
+								distAvg = distSum/count
+								datum = {}
+								datum['station'] = s1
+								datum['date'] = str(yy)+'-'+mmdd
+								datum['angle'] = angleAvg
+								datum['angle_1'] = gcData[ (i-count+n)%n ]['angle']		# for specify angle range
+								datum['angle_2'] = gcData[ (i-1+n)%n ]['angle']			# for specify angle range
+								datum['str'] = count
+								datum['fromto'] = fromto
+								datum['dist'] = distAvg
+								result.append( datum )
+					return result
 
-			outDirs = findGeneralDirections( tmp_gcData, outGCseq, minArrowGC, True )
-			inDirs = findGeneralDirections( tmp_gcData, inGCseq, minArrowGC, False )
-			print outDirs
-			print inDirs
-			break
-		break
+				outDirs = findGeneralDirections( tmp_gcData, outGCseq, minArrowGC, True )
+				inDirs = findGeneralDirections( tmp_gcData, inGCseq, minArrowGC, False )
 
+				#print outDirs
+				#print inDirs
+				def writeData(data, ):
+					tmp_count = 0
+					for rec in data:
+						output = [
+								  rec['station'],
+								  rec['date'],
+								  str(rec['angle']), 
+								  str(rec['angle_1']), 
+								  str(rec['angle_2']), 
+								  str(rec['str']), 
+								  str(rec['fromto']), 
+								  str(rec['dist'])
+								  ]
+						f.write('\n')
+						f.write(','.join(output))
+						tmp_count += 1
+					return tmp_count
+				n_year_arrows += writeData(outDirs)
+				n_year_arrows += writeData(inDirs)
+				#break
+			#break
+		print 'arrows in',yy,':', n_year_arrows
+		n_arrows += n_year_arrows
+print 'number of arrows:', n_arrows
 
